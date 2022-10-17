0b997348237e8abada015254fe1077374da51870
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2022-10-17 13:52:19 +0300

    js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,16 @@ let offset = 0;
 
 const sliderToggle = document.querySelector(".body-main-block__arrows");
 
-const propertiesMenu = document.querySelector(".deals__container");
+const zipCodeSearchForm = document.querySelector(".form-main-block");
+
+const propertiesMenu = document.querySelector(".tabs-deals__menu");
+const allPropertyButton = document.querySelector(".header-deals__link");
+const properties = document.querySelectorAll(".item-items-tab");
 
 sliderToggle.addEventListener("click", changeSlider);
 propertiesMenu.addEventListener("click", changePropertyByValue);
+allPropertyButton.addEventListener("click", changePropertyByValue);
+zipCodeSearchForm.addEventListener("click", showPropertyByZipcode);
 
 document.querySelector(".header__popup").addEventListener("click", () => {
   document.querySelector(".header__popup-icon").classList.toggle("fa-xmark");
@@ -64,12 +70,34 @@ function addArrowColor(target) {
 
 function changePropertyByValue({ target }) {
   event.preventDefault();
-  const properties = document.querySelectorAll(".item-items-tab");
   properties.forEach((property) => {
     if (target.id !== property.dataset.id && target.id !== "all-property") {
       property.classList.add("hide");
     } else {
       property.classList.remove("hide");
+    }
+  });
+}
+
+function showPropertyByZipcode(event) {
+  event.preventDefault();
+
+  const tootlip = document.querySelector(".form-main-block__tootlip");
+  const zipCodeValue = document
+    .querySelector(".form-main-block__input")
+    .value.trim();
+
+  properties.forEach((property) => {
+    if (property.dataset.zip !== zipCodeValue && zipCodeValue !== "") {
+      tootlip.classList.toggle("form-main-block__tootlip-visible");
+      tootlip.textContent = "Zipcode does not exist";
+    }
+
+    if (property.dataset.zip !== zipCodeValue) {
+      property.classList.add("hide");
+    } else {
+      property.classList.remove("hide");
+      property.scrollIntoView({ block: "center", behavior: "smooth" });
     }
   });
 }
